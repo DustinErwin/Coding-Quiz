9621e0cf6d635832653dfe64c8b996310df7e814
Galahad <derwin499@gmail.com>
2020-10-14 21:00:03 -0400

    added timer

--- assets/script.js
+++ assets/script.js
@@ -1,16 +1,23 @@
 let intro = document.querySelector(".intro");
-let quizArea = document.querySelector('.quizArea');
-let timer = document.querySelector("#timer");
+let quizArea = document.querySelector(".quizArea");
+
 let startBtn = document.getElementById("startBtn");
-let btnOne = document.querySelector('#ansOne');
-let btnTwo = document.querySelector('#ansTwo');
-let btnThree = document.querySelector('#ansThree');
-let btnFour = document.querySelector('#ansFour');
-let question = document.querySelector('#question')
+let btnOne = document.querySelector("#ansOne");
+let btnTwo = document.querySelector("#ansTwo");
+let btnThree = document.querySelector("#ansThree");
+let btnFour = document.querySelector("#ansFour");
+let question = document.querySelector("#question");
+let isWrong = document.querySelector("#isWrong");
+let quizFinish = document.querySelector(".quizFinish");
+let score = document.querySelector("#score");
+let init = document.querySelector("#init");
+let saveBtn = document.querySelector("#save");
+
+let seconds = 60;
 let count = 0;
 let countDown = 75;
 let highScores = [];
-let tally = [];
+let tally = 0;
 let teacher = ["Correct!", "Wrong!"];
 //Questions
 let questions = [
@@ -21,9 +28,9 @@ let questions = [
   "What does DOM stand for?",
   "Which preloaded function is commonly used for debugging?",
   "How do we indirectly find how long an array is?",
-  "What is short hand for plu one?",
+  "What is short hand for plus one?",
   "Events happen by using which of these?",
-  "We can keep an onclick from triggering any ouside elements by using _________.",
+  "We can keep an onclick from triggering any outside elements by using _________.",
 ];
 //Answers
 let answerPool = [
@@ -40,12 +47,7 @@ let answerPool = [
   ["prompt.log", "alert.log", "console.log", "popup.log"],
   [".push", ".length", ".width", ".end"],
   ["+=", "+1", "1+", "++"],
-  [
-    ".querySelector",
-    ".addEventListener",
-    "getElementById",
-    ".setAttribute",
-  ],
+  [".querySelector", ".addEventListener", "getElementById", ".setAttribute"],
   [
     ".preventMyfault",
     ".preventYourfault",
@@ -53,29 +55,83 @@ let answerPool = [
     "preventDefault",
   ],
 ];
+let correctAns = [0, 2, 1, 2, 0, 2, 1, 3, 1, 3];
 //Starts the quiz
 function go() {
-  intro.setAttribute('style','display: none;')
-  quizArea.setAttribute('style', 'display: flex;')
-  quiz()
+  intro.setAttribute("style", "display: none;");
+  quizArea.setAttribute("style", "display: flex;");
+  secInt = setInterval(clock, 1000);
+  quiz();
+}
 
-};
-
+//Fills quiz area with quiz data
 function quiz() {
-  
-  question.textContent = questions[count]
-  btnOne.textContent = answerPool[count][0]
-  btnTwo.textContent = answerPool[count][1]
-  btnThree.textContent = answerPool[count][2]
-  btnFour.textContent = answerPool[count][3]
-  count++
-  btnOne.addEventListener('click', quiz)
-  btnTwo.addEventListener('click', quiz)
-  btnThree.addEventListener('click', quiz)
-  btnFour.addEventListener('click', quiz)
-  if(count === 10){
-    quizArea.setAttribute('style', 'display: none;')
-  }
-};
+  console.log(count);
+  question.textContent = questions[count];
+  btnOne.textContent = answerPool[count][0];
+  btnTwo.textContent = answerPool[count][1];
+  btnThree.textContent = answerPool[count][2];
+  btnFour.textContent = answerPool[count][3];
+}
+//Collects answers from the user
+btnOne.addEventListener("click", function (event) {
+  event.stopPropagation();
+  Check(0);
+});
+btnTwo.addEventListener("click", function (event) {
+  event.stopPropagation();
+  Check(1);
+});
+btnThree.addEventListener("click", function (event) {
+  event.stopPropagation();
+  Check(2);
+});
+btnFour.addEventListener("click", function (event) {
+  event.stopPropagation();
+  Check(3);
+});
 
+//Checks Answers for correctness
+function Check(ans) {
+  if (correctAns[count] === ans) {
+    tally++;
+
+    isWrong.textContent = teacher[0];
+  } else {
+    clock(10);
+    isWrong.textContent = teacher[1];
+  }
+  count++;
+  if (count === 10) {
+    quizArea.setAttribute("style", "display: none;");
+    end();
+    return;
+  }
+
+  quiz();
+}
+function end() {
+  quizFinish.setAttribute("style", "display: flex;");
+  score.textContent = " " + tally;
+}
+function save() {
+  let scoreObj = {
+    initials: init.value,
+    score: tally,
+  };
+  highScores.push(scoreObj);
+}
+
+function clock(decrement = 1) {
+  seconds -= decrement;
+  let timer = document.querySelector("#timer");
+  timer.textContent = " " + seconds;
+  if (seconds <= 0) {
+    clearInterval(secInt);
+
+    end()
+  }
+}
+
+saveBtn.addEventListener("click", save);
 startBtn.addEventListener("click", go);
